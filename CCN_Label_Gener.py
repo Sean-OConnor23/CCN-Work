16b5c2c7b5b9bd5a400741997f6d9faf838ead33
Sean O'Connor <sfoconnor1999@gmail.com>
2023-01-15 10:43:42 -0600

    Adding Label Generation beginning script

---
+++ CCN_Label_Gener.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from PIL import Image
+
+
+class CCN_Product_Label_Generator:
+    def __init__(self) -> None:
+        pass
+    
+    def generate_template():
+        print("Success")
+        
+def main():
+    CCN_Product_Label_Generator.generate_template()
+    
+if __name__ == "__main__":
+    main()
